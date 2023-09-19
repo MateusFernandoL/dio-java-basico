01c369f81ce3c450c4eeabe55ee246e0e1cccfa8
MateusFernando <maateus.lopes.nv@hotmail.com>
2023-09-19 15:20:27 -0300

    TipoVariaveis

---
+++ tipos-e-variaveis/src/TiposEVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposEVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        byte idade = 25;
+
+        short ano = 2023;
+
+        int cep = 21070333; // Se começar com zerp, talvez tenha que ser outro tipo(String)
+
+        long cpf = 38311739012L; // Finaliza-se com "L". Se começar com zerp, talvez tenha que ser outro tipo
+                                 // (String).
+
+        float valorPi = 3.14F; // Finaliza-se com "F"
+
+        double salario = 127533;
+    }
+}
