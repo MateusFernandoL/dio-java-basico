5e631bf49219fbd6cd38e47cf2dff1b86b581338
MateusFernando <maateus.lopes.nv@hotmail.com>
2023-10-16 10:51:20 -0300

    Atualização estudos

---
+++ Tratamento de Exceçoes/src/TryCatch.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) throws Exception {
+
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome: ");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
+
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos");
+            System.out.println("Minha altura é " + altura + "cm");
+        } catch (InputMismatchException erro) {
+            System.out.println("Idade e altura são campos numéricos");
+        }
+
+    }
+}
+
+// EXCEÇÕES QUE PODEM ACONTECER
+
+// - Não informar o nome e sobrenome
+// - O valor da idade ter um caractere NÃO numérico
+// - O valor da altura ter uma virgula ou invês de ser um ponto'
